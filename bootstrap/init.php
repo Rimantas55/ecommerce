cfc80fb73a80520a0470d3af0b2ddb3bc31d2c8e
Rimantas <rdanilka@gmail.com>
2019-09-22 23:08:43 +0300

    start session if not already started

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -1,3 +1,11 @@
 <?php
 
+/**
+ * Start Session if not already started
+ */
+
+if(!isset($_SESSION)) session_start();
+
+
+// Load enviroment variable
 require_once __DIR__ . '/../app/config/_env.php';
